3735510b711a7ae3aaff8566ec8a2ed7a5a05ee4
sidglas <sidglas@hotmail.com>
2021-06-23 22:52:12 -0300

    migration adding  pass column to User

---
+++ src/database/migrations/1624492845804-AlterUserAddPassword.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterUserAddPassword1624492845804 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
